4fc1f3fe1889761724935796ae9250330b84be4d
coffey <coffey@nligo.com>
2018-11-24 15:23:37 +0800

    修改东西

--- assets/sb/sb.js
+++ assets/sb/sb.js
@@ -1,11 +1,5 @@
 global.$ = require('jquery');
 
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
 require('../sb/css/bootstrap.css');
 require('../sb/css/metisMenu.css');
 require('../sb/css/sb-admin-2.css');
